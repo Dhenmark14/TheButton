219bf246ad6eb9f7eda078e5584b96fb2bf6f711
Dhenmark14 <134914049+Dhenmark14@users.noreply.github.com>
2023-06-16 23:38:11 +0800

    Add files via upload

---
+++ button.js
@@ -0,0 +1,8 @@
+function wrong() {
+    alert("Wrong Answer");
+    document.getElementById("button");
+}
+function right () {
+    alert("You are Correct!!!");
+    document.getElementById("right");
+}
