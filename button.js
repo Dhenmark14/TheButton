23b6d9a1a917be79c32f4859611feb186b89a59f
Dhenmark14 <134914049+Dhenmark14@users.noreply.github.com>
2023-06-20 09:25:02 +0800

    Delete button.js

--- button.js
+++
@@ -1,8 +0,0 @@
-function wrong() {
-    alert("Wrong Answer");
-    document.getElementById("button");
-}
-function right () {
-    alert("You are Correct!!!");
-    document.getElementById("right");
-}
